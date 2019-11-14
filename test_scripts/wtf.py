ed47e164342722dee1eac90df33cd3ccf1eeba57
Ross Marchant <ross.g.marchant@gmail.com>
2019-11-15 09:09:49 +1100

    wtf is going on

--- test_scripts/wtf.py
+++ test_scripts/wtf.py
@@ -5,13 +5,13 @@ from miso.data.download import download_images
 params = default_params()
 
 params['type'] = 'resnet50_tl'
-params['name'] = ''
+params['name'] = 'afafd'
 params['description'] = None
 params['input_source'] = r'C:\Users\rossm\Documents\Data\Foraminifera\EndlessForams\border_removed\project_exported.xml'
 params['data_min_count'] = 40
 params['data_split'] = 0.200000
 params['data_map_others'] = False
-params['output_dir'] = r'C:\Users\rossm\Documents\Data\Foraminifera\EndlessForams\output'
+params['output_dir'] = r'C:\Users\rossm\Documents\Data\Foraminifera\EndlessForams\border_removed'
 params['save_model'] = 'frozen'
 params['save_mislabeled'] = True
 params['img_height'] = 224
@@ -21,7 +21,5 @@ params['use_augmentation'] = True
 params['use_class_weights'] = True
 params['alr_epochs'] = 10
 params['batch_size'] = 64
-params['delete_mmap_files'] = False
-params['mmap_directory'] = None
 
 model, vector_model, data_source, result = train_image_classification_model(params)
