0598d9d93759dac99f1f5fffb9b15b2fd205de65
Ross Marchant <ross.g.marchant@gmail.com>
2019-11-18 17:31:55 +1100

    weights file in temporary directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='miso',
-    version='2.0.9',
+    version='2.0.10',
     packages=['miso', 'miso.data', 'miso.stats', 'miso.save', 'miso.layers', 'miso.models', 'miso.training'],
     install_requires=['image-classifiers>=1.0.0', 'lxml', 'matplotlib', 'numpy', 'pandas', 'Pillow',
                       'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill'],
