fa0e662f94a91975c4e5f2b2e91fdc25d2526555
Ross Marchant <ross.g.marchant@gmail.com>
2020-01-29 15:48:05 +1100

    requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0.13',
     packages=['miso', 'miso.data', 'miso.stats', 'miso.save', 'miso.layers', 'miso.models', 'miso.training'],
     install_requires=['image-classifiers>=1.0.0', 'lxml', 'matplotlib', 'numpy', 'pandas', 'Pillow',
-                      'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill'],
+                      'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill', 'flask'],
     url='',
     license='',
     author='Ross Marchant',
