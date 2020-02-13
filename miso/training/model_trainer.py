8aa9d7f7b4f460d6dcb9d274618a8f801c0d9a9e
Ross Marchant <ross.g.marchant@gmail.com>
2020-02-14 10:16:46 +1100

    minst test

--- miso/training/model_trainer.py
+++ miso/training/model_trainer.py
@@ -93,6 +93,27 @@ def train_image_classification_model(params: dict, data_source: DataSource = Non
     params['num_classes'] = data_source.num_classes
 
     if cnn_type.endswith('tl'):
+
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10)
+        ])
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        model.compile(optimizer='adam',
+                      loss=loss_fn,
+                      metrics=['accuracy'])
+        model.fit(x_train, y_train, epochs=5)
+        model.evaluate(x_test, y_test, verbose=2)
+        probability_model = tf.keras.Sequential([
+            model,
+            tf.keras.layers.Softmax()
+        ])
+
         start = time.time()
         # Create Vectors -----------------------------------------------------------------------------------------------
         # Note that the images are scaled internally in the network to match the expected preprocessing
