f889927b6fab048b623847054f5914185bc6e7ff
Ross Marchant <ross.g.marchant@gmail.com>
2020-03-26 14:56:22 +1000

    Bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='miso',
-    version='2.0.18',
+    version='2.0.19',
     packages=['miso', 'miso.data', 'miso.deploy', 'miso.layers', 'miso.models', 'miso.stats', 'miso.training'],
     install_requires=['image-classifiers>=1.0.0', 'lxml', 'matplotlib', 'numpy', 'pandas', 'Pillow',
                       'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill', 'flask'],
