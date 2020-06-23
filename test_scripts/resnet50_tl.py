84b2b1a0448eeaab99705f75fd0e57924ee07dac
Ross Marchant <ross.g.marchant@gmail.com>
2020-06-24 08:26:37 +1000

    test

--- test_scripts/resnet50_tl.py
+++ test_scripts/resnet50_tl.py
@@ -18,7 +18,7 @@ params['description'] = None
 # - resnet34
 # - resnet50
 # - resnet50_tl     (resnet50 using transfer learning)
-params['type'] = 'efficientnetB0'
+params['type'] = 'resnet50_cyclic_tl'
 params['filters'] = 4
 
 # The input dimensions of the image.
@@ -47,6 +47,7 @@ params['use_augmentation'] = False
 # Input data source
 # Can be local directory or URL to zip file
 params['input_source'] = r'D:\Datasets\Foraminifera\fifth_with_background.xml'
+params['input_source'] = r"C:\Users\marchanr\OneDrive\Datasets\Pollen\pollen_all"
 # Minimum number of images per class for that class to be included.
 # Classes with only a few images are not worth using in training.
 params['data_min_count'] = 40
