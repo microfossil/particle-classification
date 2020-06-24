35d7db5f5ed9890f89ef918c6b9f78e79778fc77
Ross Marchant <ross.g.marchant@gmail.com>
2020-06-25 08:17:54 +1000

    Try run on deep weeds

--- test_scripts/resnet50_tl.py
+++ test_scripts/resnet50_tl.py
@@ -18,14 +18,14 @@ params['description'] = None
 # - resnet34
 # - resnet50
 # - resnet50_tl     (resnet50 using transfer learning)
-params['type'] = 'resnet50_cyclic_tl'
+params['type'] = 'efficientnetB4'
 params['filters'] = 4
 
 # The input dimensions of the image.
 # For transfer learning, the dimensions will automatically be set to the appropriate size for the network.
 params['img_height'] = 128
 params['img_width'] = 128
-params['img_channels'] = 1
+params['img_channels'] = 3
 
 # Training
 # Batch size is number of images presented per training iteration.
@@ -48,13 +48,14 @@ params['use_augmentation'] = False
 # Can be local directory or URL to zip file
 params['input_source'] = r'D:\Datasets\Foraminifera\fifth_with_background.xml'
 params['input_source'] = r"C:\Users\marchanr\OneDrive\Datasets\Pollen\pollen_all"
+params['input_source'] = r"D:\Datasets\Weeds\DeepWeedsConverted"
 # Minimum number of images per class for that class to be included.
 # Classes with only a few images are not worth using in training.
 params['data_min_count'] = 40
 # Weight the classes by count
 params['use_class_weights'] = True
 # Fraction of images used for testing to calculate accuracy etc
-params['data_split'] = 0.25
+params['data_split'] = 0.20
 
 # Output
 params['output_dir'] = r'output/'
