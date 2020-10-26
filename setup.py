1c266e9b067e928f7fdf66a14dbd1860589001c8
Ross Marchant <ross.g.marchant@gmail.com>
2020-10-26 13:05:05 +1000

    Update setup ready for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,42 @@
 from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='miso',
     version='2.2.1',
-    packages=['miso', 'miso.data', 'miso.deploy', 'miso.layers', 'miso.models', 'miso.stats', 'miso.training'],
-    install_requires=['image-classifiers>=1.0.0', 'lxml', 'matplotlib', 'numpy', 'pandas', 'Pillow',
-                      'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill', 'flask', 'tqdm', 'xlrd'],
-    url='',
-    license='',
+    description='Python scripts for training CNNs for particle classification',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ross Marchant',
     author_email='ross.g.marchant@gmail.com',
-    description=''
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+    ],
+    keywords='microfossil, cnn',
+    python_requires='>=3.6, <4',
+    packages=['miso', 'miso.data', 'miso.deploy', 'miso.layers', 'miso.models', 'miso.stats', 'miso.training'],
+    install_requires=['image-classifiers>=1.0.0',
+                      'lxml',
+                      'matplotlib',
+                      'numpy',
+                      'pandas',
+                      'Pillow',
+                      'scikit-image',
+                      'scikit-learn',
+                      'scipy',
+                      'segmentation-models',
+                      'dill',
+                      'flask',
+                      'tqdm',
+                      'xlrd'],
+    url='',
+    license='',
+    project_urls={  # Optional
+        'Source': 'https://github.com/microfossil/particle-classification',
+        'Paper': 'https://jm.copernicus.org/articles/39/183/2020/',
+    },
 )
