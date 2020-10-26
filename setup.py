c24a61e6ce0a6b7e01ca5ed492f267e9979db933
Ross Marchant <ross.g.marchant@gmail.com>
2020-10-26 13:06:52 +1000

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='miso',
-    version='2.2.1',
+    version='2.3.0',
     description='Python scripts for training CNNs for particle classification',
     long_description=long_description,
     long_description_content_type='text/markdown',
