b63bf310bc804f43852bc8bfe9bf3aee0ae94647
Ross Marchant <ross.marchant@my.jcu.edu.au>
2020-11-20 23:24:42 +1000

    Version number increase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='miso',
-    version='2.2.1',
+    version='2.2.2',
     packages=['miso', 'miso.data', 'miso.deploy', 'miso.layers', 'miso.models', 'miso.stats', 'miso.training'],
     install_requires=['image-classifiers>=1.0.0', 'lxml', 'matplotlib', 'numpy', 'pandas', 'Pillow',
                       'scikit-image', 'scikit-learn', 'scipy', 'segmentation-models', 'dill', 'flask', 'tqdm', 'xlrd'],
