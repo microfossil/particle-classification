8f9351788f361dc97aabc623ed61f8c0331ab855
Ross Marchant <ross.marchant@my.jcu.edu.au>
2020-11-22 18:25:03 +1000

    vec

--- miso/training/trainer.py
+++ miso/training/trainer.py
@@ -139,7 +139,7 @@ def train_image_classification_model(tp: MisoParameters):
             print("- class balancing using random over sampling")
 
         v = model_tail.predict(vectors[0:1])
-            print(v[0, :10])
+        print(v[0, :10])
 
         # Train
         history = model_tail.fit_generator(train_gen.create(),
