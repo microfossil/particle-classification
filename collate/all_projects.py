00277a82c539bb99a2a871ab0083db1bd23abaaf
Ross Marchant <ross.marchant@data61.csiro.au>
2022-01-10 11:16:03 +1000

    Collate project data

---
+++ collate/all_projects.py
@@ -0,0 +1,74 @@
+import os
+from collections import OrderedDict, namedtuple
+from pathlib import Path
+import lxml.etree as ET
+import pandas as pd
+import numpy as np
+
+ClsInfo = namedtuple('ClsInfo', 'cls score val_by project')
+
+
+def parse_xml(xml_filename, data):
+    project = ET.parse(xml_filename).getroot()
+
+    if data is None:
+        data = OrderedDict()
+
+    images_xml = project.find('images')
+    for i, image_xml in enumerate(images_xml.iter('image')):
+        relfile = image_xml.find('source').find('filename').text
+        if os.path.isabs(relfile):
+            absfile = relfile
+        else:
+            absfile = os.path.abspath(os.path.join(os.path.dirname(xml_filename), relfile))
+        if os.path.isfile(absfile) is False:
+            continue
+
+        cls_names = []
+        cls_scores = []
+        cls_base = image_xml.find('classifications')
+        for cls_val in cls_base.iter('classification'):
+            cls_names.append(cls_val.find('code').text)
+            cls_scores.append(float(cls_val.find('value').text))
+        if absfile not in data:
+            data[absfile] = []
+        info = ClsInfo(cls_names[np.argmax(cls_scores)],
+                       np.max(cls_scores),
+                       image_xml.find('val_by').text,
+                       xml_filename)
+        data[absfile].append(info)
+
+    return data
+
+    # for taxon_xml in project.find('taxons').iter('taxon'):
+    #     if taxon_xml.find('isClass').text == 'true':
+    #         cls_labels.append(taxon_xml.find('code').text)
+    # cls_labels = sorted(cls_labels)
+    #
+    # df = pd.DataFrame({'filenames': filenames, 'cls': cls})
+    # for label in cls_labels:
+    #     filenames_dict[label] = df.filenames[df.cls == label]
+    # return filenames_dict
+
+
+SOURCE_DIR = "/media/mar76c/DATA/Datasets/Texture"
+
+xmls = Path(SOURCE_DIR).rglob("*.xml")
+data = None
+for xml in xmls:
+    print(xml)
+    with open(xml, "r") as pfn:
+        pfn.readline()
+        line = pfn.readline()
+        if line.startswith("<project"):
+            data = parse_xml(str(xml), data)
+
+counts = {}
+for key, value in data.items():
+    for info in value:
+        if info.cls not in counts:
+            counts[info.cls] = 0
+        counts[info.cls] += 1
+        print(f"{key},{info.cls},{info.score},{info.val_by},{info.project}")
+
+print(counts)
