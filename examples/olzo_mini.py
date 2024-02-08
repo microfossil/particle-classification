b9ae814c0bd3e6103974a43385aba070d063e935
Ross Marchant <ross.g.marchant@gmail.com>
2024-02-08 12:05:05 +1100

    Downgrade onnx so that protobuf error is not happening

--- examples/olzo_mini.py
+++ examples/olzo_mini.py
@@ -44,7 +44,7 @@ tp.dataset.memmap_directory = None
 # - resnet[18,34,50]
 # - vgg[16,19]
 # - efficientnetB[0-7]
-tp.cnn.id = "convnexttiny"
+tp.cnn.id = "convnexttiny_tl"
 # Input image shape, set to None to use default size ([128, 128, 1] for custom, [224, 224, 3] for others)
 tp.cnn.img_shape = [128, 128, 1]
 # Input image colour space [greyscale/rgb]
