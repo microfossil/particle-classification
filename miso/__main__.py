7ac83b47521a3ab59e50d3493904dca2afae46a7
Ross Marchant <ross.g.marchant@gmail.com>
2024-02-08 22:38:11 +1100

    Typo in cli

--- miso/__main__.py
+++ miso/__main__.py
@@ -19,11 +19,11 @@ def cli():
 @click.option('--in_samples/--no-in_samples', '-f', default=False, show_default=True, help='Set this flag if images are stored in subfolders, using the subfolder names as sample labels.')
 @click.option('--sample', '-s', type=str, default='unknown', show_default=True, help='Default sample name if not using subdirectories.')
 @click.option('--unsure_threshold', 'u', type=float, default=0.0, show_default=True, help='Threshold below which predictions are considered unsure.')
-def classify_folder(model, folder, output, batch_size, in_samples, sample, unsure_threshold):
+def classify_folder(model, input, output, batch_size, in_samples, sample, unsure_threshold):
     """
     Classify images in a folder and output the results to a CSV file.
     """
-    classify_folder(model, folder, output, batch_size, in_samples, sample, unsure_threshold)
+    classify_folder(model, input, output, batch_size, in_samples, sample, unsure_threshold)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='miso',
-    version='3.1.11',
+    version='3.1.12',
     description='Python scripts for training CNNs for particle classification',
     long_description=long_description,
     long_description_content_type='text/markdown',
